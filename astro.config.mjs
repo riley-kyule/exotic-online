4ea9ab27e66cd068062f7cdc88dae452f37b9397
riley-kyule <phillipkyule@gmail.com>
2025-08-25 15:50:55 +0300

    chore: configure Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+	integrations: [mdx()],
+	adapter: vercel()
 });
